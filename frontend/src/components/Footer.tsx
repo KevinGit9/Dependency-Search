8a28fd8b9fcc09721c815150fe66c2ed2a143913
KevinGit9 <1037132@hr.nl>
2023-06-08 14:50:39 +0200

    homepage styling

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,15 +1,40 @@
 import React from "react";
-import "./Footer.css"
+import "./Footer.css";
 
 function Footer() {
     return (
         <footer className="footer">
-            <h1> Team Random </h1>
-            <p> This application is a prototype to search for specific dependencies in projects by looking through SBOMs. </p>
+            <div className="footerHeader">
+                <a
+                    href="https://github.com/KevinGit9/ProjectD"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="github-link"
+                >
+                    <img
+                        src="github-logo.png" // Replace with your actual GitHub logo URL
+                        alt="GitHub Logo"
+                    />
+                </a>
+                <div className="footerText">
+                    <h1> Team Random </h1>
+                    <p> This application is a prototype to search for specific dependencies in projects by looking through SBOMs. </p>
+                </div>
+            </div>
             <div style={{ height: "200px" }}>
+                <div className="footerPanel">
+                    <div className="developers">
+                        <h2>Developers:</h2>
+                        <ul>
+                            <li>Anton Shi</li>
+                            <li>Kevin Shi</li>
+                            <li>Tobias Zelders</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </footer>
     );
-};
+}
 
 export default Footer;
