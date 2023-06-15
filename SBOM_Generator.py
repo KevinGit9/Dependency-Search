d2fa54b806c5f4726e085d550a6611b4f1df98f8
Ant0n8 <a.shi2012@hotmail.com>
2023-06-15 14:17:53 +0200

    Update SBOM_Generator.py

--- SBOM_Generator.py
+++ SBOM_Generator.py
@@ -7,20 +7,19 @@ images = client.images.list("")
 sbom_id = 0
 
 # Create the directory if it doesn't exist
-directory = os.path.abspath(os.getcwd()) #os.path.expanduser("~/Documents/SBOM")\
+directory = os.path.abspath(os.getcwd())
 final_directory = os.path.join(directory, r'SBOM')
-print(final_directory)
+
 if not os.path.exists(final_directory):
     os.mkdir(final_directory)
 
 for image in images:
     # File location
-    file_name = f"{image.tags[0].split(':')[1]}_SBOM_{sbom_id}.json"
+    file_name = f"{image.tags[0].split('/')[-1].replace(':', '_')}_SBOM_{sbom_id}.json"
     file_path = os.path.join(final_directory, file_name)
 
     # Command to run
     command = ["syft", "-o", "cyclonedx-json", "--file", file_path, image.tags[0]]
-    print(image.tags[0])
 
     # Run the command
     process = subprocess.Popen(command)
